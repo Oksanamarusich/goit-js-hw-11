67f9fa4a4fe789dd21d85bfda05873a0d6e1404d
Oksanamarusich <128741626+Oksanamarusich@users.noreply.github.com>
2023-08-31 21:23:40 +0000

    Deploying to gh-pages from @ Oksanamarusich/goit-js-hw-11@9d29e1cfac9da16067514d24f4f295f1b6c9a646 🚀

--- index.0a08d474.js
+++
@@ -1,2 +0,0 @@
-var elem={searchForm:document.querySelector(".search-form"),gallery:document.querySelector("gallery")};function handlerSearch(e){e.preventDefault(),console.dir(e.currentTarget);var r=e.currentTarget.elements.searchQuery;console.log(r.value)}elem.searchForm.addEventListener("submit",handlerSearch);
-//# sourceMappingURL=index.0a08d474.js.map
